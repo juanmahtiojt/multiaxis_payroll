fedc46f86bf4872f4adb7117d665b78a7645e38a
juamahtiojt-creator <juanmahtiojt@gmail.com>
2025-09-02 09:15:59 +0800

    Update testing

---
+++ my_project/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Payroll System</title>
+</head>
+<body>
+
+<h3 class="text-center">
+    <img src="images/MULTI-removebg-preview.png" style="width: 100px; height: 100px;">
+</h3>
+
+</body>
+</html>
+<!-- gi -->
